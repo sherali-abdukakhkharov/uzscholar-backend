7a9ff877a964864dce0a21251a62add0778bf9d6
LionAli <sherali.abdukakhkharov@gmail.com>
2025-07-11 00:45:20 +0500

    fix: remove unused imports

--- src/dissertations/dissertation.service.ts
+++ src/dissertations/dissertation.service.ts
@@ -1,7 +1,6 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { DissertationRepo } from './dissertation.repo';
 import { GetAllDissertationsDto } from './dtos/get-all.dto';
-import { krillToLatin, latinToKrill } from '@shared/utils/translate';
 
 @Injectable()
 export class DissertationService {
